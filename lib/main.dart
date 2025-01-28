17b34b76b70723d7f9571fba469a654a5bfa406a
Abdulrasol <iraq.rsol@gmail.com>
2025-01-28 14:34:09 +0300

    fix entry point

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        initialRoute: '/preivew_screen',
+        initialRoute: '/',
         routes: {
           '/': (context) => const HomePage(),
           '/form': (context) => const FromWidget(),
